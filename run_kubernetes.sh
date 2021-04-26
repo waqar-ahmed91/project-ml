b0d579878a9b9fe54072650f385948893672a0c4
Waqar Ahmed <waqar.nu@gmail.com>
2021-04-26 18:38:18 +1000

    updated files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,15 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath = vickydavid/project4
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl run project4 --image=vickydavid/project4 --port=80
+sleep 10
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward project4 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath = vickydavid/project4
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username vickydavid
+docker tag project4 vickydavid/project4:project4
 
 # Step 3:
 # Push image to a docker repository
+docker push vickydavid/project4:project4
